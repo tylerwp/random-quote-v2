db197a8defb2e2ac0626f556ca0ec7fea4ca6007
tylerwp <tylerwp@gmail.com>
2016-08-11 17:02:44 -0400

    getRandomQuote function complete

---
+++ js/quotes.js
@@ -0,0 +1,67 @@
+/*
+  Javascript quotes array of objects  
+*/
+
+var quotes = [
+    {
+        quote:'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
+        source:'Benjamin Franklin',
+        wikipedia:'https://en.wikipedia.org/wiki/Benjamin_Franklin',
+        Born:'January 17, 1706',
+        Died:'April 17, 1790'
+
+    },
+    {
+        quote:'Quality means doing it right when no one is looking.',
+        source:'Henry Ford',
+        wikipedia:'https://en.wikipedia.org/wiki/Henry_Ford',
+        citation:'Detroit News 1927',
+        year:1927,
+        Born:'July 30, 1863',
+        Died:'April 7, 1947'
+    },
+    {
+        quote:'The true sign of intelligence is not knowledge but imagination.',
+        source:'Albert Einstein',  
+        wikipedia:'https://en.wikipedia.org/wiki/Albert_Einstein',      
+        year:1945,
+        Born:'March 14, 1879',
+        Died:'April 18, 1955'
+    },
+    {
+        quote:'The secret of getting ahead is getting started.',
+        source:'Mark Twain',  
+        wikipedia:'https://en.wikipedia.org/wiki/Mark_Twain',  
+        Born:'February 7, 1871',
+        Died:'April 21, 1910'
+    },
+    {
+        quote:'Those who cannot understand how to put their thoughts on ice should not enter into the heat of debate.',
+        source:'Friedrich Nietzsche',  
+        wikipedia:'https://en.wikipedia.org/wiki/Friedrich_Nietzsche',
+        citation:'Leipziger Volkszeitung',
+        Born:'October 15 1844',
+        Died:'August 25 1900'
+    }
+]
+
+
+
+
+
+/*
+    Create an array of JavaScript objects --
+        Create an array of JavaScript objects to hold the data for your quotes. 
+        Name array [quotes]. 
+        The [quotes] array should be accessible in the global scope.
+
+        Each quote object should have the following properties:
+        {
+            quote: 'contains a string: the text of the quote to display on the page',
+            source: 'contains a string identifying the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb"',
+            *citation: 'optional...contains a string identifying the publication the quote appears in. For example, "Famous Anonymous Jokes". If there is no known publication, then do not include this property on the object.',
+            *year: 1997 //property which contains a number identifying the date of the quote. For example, 1997. If there is no known date, then do not include this property on the object.//
+        }
+        Q:How to handle a not included property in a object?
+        *optional
+*/
--- js/script.js
+++ js/script.js
@@ -2,3 +2,81 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+//Array to prevent displaying a random quote more than once until ALL quotes from the array have been displayed. 
+var preventQuotes = [];
+
+//get a random quote from the quotes array.
+function getRandomQuote(){
+    //get the index of a quote using the Math.random function on the quotes array.
+    var randomQuote = -1;
+    //loop until its finds a quite that has not been displayed  
+    while(true){
+        //get random number
+        randomQuote = Math.floor(Math.random()*quotes.length);
+        //check preventQuotes for randomQuote
+        if(preventQuotes.indexOf(randomQuote) != -1){                        
+            console.log('Quote allready been displayed.');
+            //clear preventQuotes if all qoutes have been displayed.
+            if(quotes.length === preventQuotes.length){
+                preventQuotes = [];
+                console.log('Clearing preventQuotes');
+            }
+        }else{
+            //add to preventQuotes
+            preventQuotes.push(randomQuote);            
+            break;
+        }
+    }
+    //console.log(preventQuotes);
+    //create a new object from quotes with the new index value
+    var QuoteObj = quotes[randomQuote]; 
+    //return the object
+    return QuoteObj;
+}
+
+
+
+/* 
+        Q:How to handle a not included property in a object?        
+*/
+
+
+/*
+    function printQuote -- 
+        Create a function named printQuote which follows these rules:
+
+        - printQuote calls the getRandomQuote function and stores the returned quote object in a variable
+
+        - printQuote constructs a string using the different properties of the quote object using the following HTML 
+        template: <p class="quote"> [quote here] </p> <p class="source"> [source here] <span class="citation"> [citation here] </span> <span class="year"> [year here] </span> </p>
+
+        - printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
+
+        - printQuote displays the final HTML string to the page. You can use the following JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
+         
+         <p class="quote">You can do anything but not everything</p>
+         <p class="source">David Allen<span class="citation">Making It All Work</span><span class="year">2009</span></p>
+
+*/
+function printQuote(){
+    document.getElementById(uote-box).innerHTML = "";
+}
+
+
+/*
+    Extra Credit --
+        Add more properties to the quote object. For example, a tags property could include a list of "tags" like -- "humor", "business", "politics" -- to categorize each quote.
+
+        Randomly change the background color of the page, when the quote changes
+
+        Don't display a random quote more than once until ALL quotes from the array have been displayed.
+
+        Refresh the quote after a set amount of time. For example, every 30 seconds, make a new quote appear. (You can use the setInterval() or setTimeout() method to do this -- see the links in the Project Resources listing.)
+
+*/
+
+
+
+
+
